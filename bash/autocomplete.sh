30d1861bbaa1826d695dff2a1141a9f387fe8416
Jorge Camarero Vera <jorge.camarero@cern.ch>
2018-05-09 12:54:14 +0000

    Script to install bash-complete

---
+++ bash/autocomplete.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+mkdir ~/Repos
+cd ~/Repos
+git clone https://github.com/scop/bash-completion.git
+cd bash-completion
+git checkout v2.8
+make distclean
+autoreconf -i  # if not installing from prepared release tarball
+./configure
+make -j4
+make check # optional, requires dejagnu and tcllib
+sudo make install # as root
