f2b6267289d6d7bcff8907152987ea26d02790d1
Jorge Camarero Vera <jorge.camarero@cern.ch>
2018-05-09 14:13:57 +0000

    Script to install tmux

---
+++ tmux/tmuxInstall.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+mkdir ~/Repos
+cd ~/Repos
+git clone https://github.com/tmux/tmux.git
+cd bash-completion
+git checkout v2.7
+make distclean
+sh autogen.sh
+./configure
+make -j4
+make check # optional, requires dejagnu and tcllib
+sudo make install # as root
