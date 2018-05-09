27c5683dc15e1bdbe808ccb3b228e3bf75a1beeb
Jorge Camarero Vera <jorge.camarero@cern.ch>
2018-05-09 11:45:43 +0000

    Fix python problems with the script for the vim installation

--- vim/vimInstall.sh
+++ vim/vimInstall.sh
@@ -11,23 +11,15 @@ cd ~/Repos
 git clone https://github.com/vim/vim.git
 cd vim
 git checkout v8.0.1803
-./configure --with-features=huge \
-	    --enable-multibyte \
-							--enable-rubyinterp=yes \
-							--enable-pythoninterp=yes \
-							--with-python-config-dir=/usr/lib/python2.7/config \
-							--enable-python3interp=yes \
+make distclean
+./configure						--enable-python3interp=yes \
 							--with-python3-config-dir=/usr/lib/python3.5/config \
-							--enable-perlinterp=yes \
-							--enable-luainterp=yes \
-							--enable-gui=gtk2 \
-							--enable-cscope \
 							--prefix=/usr
 cd ~/Repos/vim
-make 
+make -j4
 sudo make install
 
-sudo update-alternatives --install /usr/bin/editor editor /usr/local/bin/vim 1
-sudo update-alternatives --set editor /usr/local/bin/vim
-sudo update-alternatives --install /usr/bin/vi vi /usr/local/bin/vim 1
-sudo update-alternatives --set vi /usr/local/bin/vim
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
+sudo update-alternatives --set editor /usr/bin/vim
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
+sudo update-alternatives --set vi /usr/bin/vim
